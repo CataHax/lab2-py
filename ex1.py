5e2d74806eac9a0c116ea3cdadfd986095e6f0d3
CataHax <84965028+CataHax@users.noreply.github.com>
2021-05-30 09:27:48 +0200

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,28 @@
+# Input two integers
+a=int(input("enter an integer number:"))
+b=int(input("enter another integer number:"))
+
+# This way
+sum=a+b
+print("the sum is:", sum)
+
+division=a/b
+print("the division is:", division)
+
+difference=a-b
+print("the difference is:", difference)
+
+# Or this way. Both work
+print("the product is:", a*b)
+
+
+print("the average is:", sum/2)
+
+
+print("the absolute value is:",abs(a-b) )
+
+
+print("the min is:",min(a,b) )
+
+
+print("the max is:", max(a,b))
