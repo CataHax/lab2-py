82420f2ab198b24cfbd2e487f43fefead27664f7
CataHax <84965028+CataHax@users.noreply.github.com>
2021-05-30 09:29:08 +0200

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# Insert numbers
+x=int(input("Enter a five-digit integer number:"))
+
+# The operation has to put space in between the numbers
+x_str = str(x)
+
+
+print(x_str[0]," ",x_str[1]," ",x_str[2]," ",x_str[3]," ",x_str[4])
